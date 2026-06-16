15f8a12020ce9be353b3535eb6920d33c36973ff
Kyriaki10 <kyriakinikolaou10@gmail.com>
2026-06-16 17:54:11 +0300

    Fixed Poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RED.java
@@ -15,36 +15,36 @@ public class RED extends BaseAutonomous {
 
     @Override
     protected Pose getShootingPose() {
-        return new Pose(-30, -30, Math.toRadians(45));
+        return new Pose(-25, -25, Math.toRadians(45));
     }
 
     @Override
     protected Pose getPos1() {
-        return new Pose(-23, -40, Math.toRadians(0));
+        return new Pose(-15, -45, Math.toRadians(0));
     }
 
     @Override
     protected Pose getPos1Forward() {
-        return new Pose(3, -40, Math.toRadians(0));
+        return new Pose(10, -45, Math.toRadians(0));
     }
 
     @Override
     protected Pose getPos2() {
-        return new Pose(-22, -60, Math.toRadians(0));
+        return new Pose(-15, -66, Math.toRadians(0));
     }
 
     @Override
     protected Pose getPosgate() {
-        return new Pose(-22, -59, Math.toRadians(0));
+        return new Pose(15, -55, Math.toRadians(0));
     }
 
     @Override
     protected Pose getPosgateready() {
-        return new Pose(-22, -59, Math.toRadians(0));
+        return new Pose(0, -55, Math.toRadians(0));
     }
 
     @Override
     protected Pose getPos2Forward() {
-        return new Pose(2, -60, Math.toRadians(0));
+        return new Pose(10, -66, Math.toRadians(0));
     }
 }
